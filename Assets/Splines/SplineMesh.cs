34ab1b46ab5f62ae030c1b03e0376d676b31326c
HARITANMAI <g013000m@student.staffs.ac.uk>
2024-07-11 17:00:13 +0530

    TestMesh: Made a struct 'OrientedPoint'

--- Assets/Splines/SplineMesh.cs
+++ Assets/Splines/SplineMesh.cs
@@ -27,15 +27,15 @@ public class SplineMesh : MonoBehaviour
             GetPos(2),
             Color.white, EditorGUIUtility.whiteTexture, 1f);
 
-        Vector3 testPoint = GetBezierPoint(t);
-        Quaternion testOrientation = GetBezierOrientation(t);
+        OrientedPoint testPoint = GetBezierPoint(t);
+        //Quaternion testOrientation = GetBezierOrientation(t);
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(testPoint, 0.2f);
-        Handles.PositionHandle(testPoint, testOrientation);
+        Gizmos.DrawSphere(testPoint.pos, 0.2f);
+        Handles.PositionHandle(testPoint.pos, testPoint.rot);
         Gizmos.color = Color.white;
     }
 
-    Vector3 GetBezierPoint(float t)
+    OrientedPoint GetBezierPoint(float t)
     {
         //Have the points which you need to evaluate through
         Vector3 p0 = GetPos(0);
@@ -52,31 +52,18 @@ public class SplineMesh : MonoBehaviour
         Vector3 d = Vector3.Lerp(a, b, t);
         Vector3 e = Vector3.Lerp(b, c, t);
 
+        Vector3 pos = Vector3.Lerp(d, e, t);
+        Vector3 tangent = (e - d).normalized;
+
+        return new OrientedPoint(pos,tangent);
         //returning the point which forms the berzier curve
-        return Vector3.Lerp(d ,e , t);
+        //return Vector3.Lerp(d ,e , t);
     }
 
-    Vector3 GetBezierTangent(float t)
-    {
-        Vector3 p0 = GetPos(0);
-        Vector3 p1 = GetPos(1);
-        Vector3 p2 = GetPos(2);
-        Vector3 p3 = GetPos(3);
+    //Quaternion GetBezierOrientation(float t)
+    //{
+    //    Vector3 tangent = GetBezierTangent(t);
 
-        Vector3 a = Vector3.Lerp(p0, p1, t);
-        Vector3 b = Vector3.Lerp(p1, p2, t);
-        Vector3 c = Vector3.Lerp(p2, p3, t);
-
-        Vector3 d = Vector3.Lerp(a, b, t);
-        Vector3 e = Vector3.Lerp(b, c, t);
-
-        return (e-d).normalized;
-    }
-
-    Quaternion GetBezierOrientation(float t)
-    {
-        Vector3 tangent = GetBezierTangent(t);
-
-        return Quaternion.LookRotation(tangent);
-    }
+    //    return Quaternion.LookRotation(tangent);
+    //}
 }
